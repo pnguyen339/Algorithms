a8e0ce81505b369f829a5a8d53b8aed9775229dc
Peter Nguyen <pnguyen339@gmail.com>
2017-06-25 23:45:21 -0500

    finished

--- Lab1/src/Program1.java
+++ Lab1/src/Program1.java
@@ -3,8 +3,9 @@
  * EID: <your EID>
  */
 
+import java.util.HashMap;
 import java.util.Vector;
-import java.util.LinkedList;
+
 
 /**
  * Your solution goes in this class.
@@ -25,9 +26,111 @@ public class Program1 extends AbstractProgram1 {
      * project documentation to help you with this.
      */
     public boolean isStableMatching(Matching given_matching) {
-        /* TODO implement this function */
-        return false; /* TODO remove this line */
+    	Vector<Vector<Integer>> landlordPref = given_matching.getLandlordPref();
+		Vector<Vector<Integer>> tenantPref = given_matching.getTenantPref();
+		Vector<Integer> match = given_matching.getTenantMatching();
+		Vector<Vector<Integer>> landlordOwn = given_matching. getLandlordOwners();
+		
+		
+		
+		if(landlordPref.size() != tenantPref.size()) {
+			
+			Vector<Vector<Integer>> landlordPref2 = new Vector<Vector<Integer>>();
+			
+			for(int i = 0; i < given_matching.getTenantCount(); i ++) {
+				int y = 0;
+				while(y < landlordPref.size()) {
+					if(landlordOwn.get(y).contains(i)) {
+						landlordPref2.add(landlordPref.get(y));
+					}
+					y++;
+				}
+			}
+			
+			landlordPref = landlordPref2;
+			
+		}
+		
+		for(int i = 0; i < given_matching.getTenantCount(); i++){
+			int Jmat = match.get(i).intValue();
+			int Wind = landlordPref.get(Jmat).indexOf(i);
+			
+			for(int y = 1; y <= Wind; y++){
+				
+				int Wpref = landlordPref.get(Jmat).get(Wind-y);
+				if(tenantPref.get(Wpref).indexOf(Jmat) < 
+					tenantPref.get(Wpref).indexOf(match.get(Wpref))){
+					return false;
+				}
+
+			}
+		}
+
+		return true; 
     }
+    
+    
+    HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+    
+    
+    public int setMatch(Matching given_matching, Vector<Integer> match, int i){
+		
+    	Vector<Vector<Integer>> landlordPref = given_matching.getLandlordPref();
+		Vector<Vector<Integer>> tenantPref = given_matching.getTenantPref();
+		Vector<Vector<Integer>> landlordOwn = given_matching. getLandlordOwners();
+		
+		if(landlordPref.size() != tenantPref.size()) {
+			
+			Vector<Vector<Integer>> landlordPref2 = new Vector<Vector<Integer>>();
+			
+			for(int i1 = 0; i1 < given_matching.getTenantCount(); i1 ++) {
+				int y1 = 0;
+				while(y1 < landlordPref.size()) {
+					if(landlordOwn.get(y1).contains(i1)) {
+						landlordPref2.add(landlordPref.get(y1));
+					}
+					y1++;
+				}
+			}
+			
+			landlordPref = landlordPref2;
+			
+		 }
+		
+		int rank = 1;
+		boolean run = true;
+		int appPoss = tenantPref.get(i).indexOf(rank);
+		while(run) {
+			
+			if(appPoss == -1){
+				rank++;
+				appPoss = tenantPref.get(i).indexOf(rank);
+			}
+			else if(hm.containsKey(appPoss) == false) {
+				hm.put(appPoss, i);
+				run = false;
+			}
+			else if(hm.containsKey(appPoss) == true) {
+				int tenantRev = (int) hm.get(appPoss);
+				if(landlordPref.get(appPoss).get(tenantRev) > landlordPref.get(appPoss).get(i)) {
+					hm.put(appPoss, i);
+					return tenantRev;
+				}
+				else
+					appPoss = tenantPref.get(i).indexOf(rank, appPoss+1);
+			}
+			else
+				appPoss = tenantPref.get(i).indexOf(rank, appPoss+1);
+			
+		}
+		
+		
+		
+		
+		
+		return -1;
+	}
+
 
     /**
      * Determines a solution to the Stable Matching problem from the given input
@@ -37,7 +140,22 @@ public class Program1 extends AbstractProgram1 {
      * @return A stable Matching.
      */
     public Matching stableMatchingGaleShapley(Matching given_matching) {
-        /* TODO implement this function */
-        return null; /* TODO remove this line */
+    	Vector<Integer> match = new Vector<Integer>(given_matching.getTenantCount());
+    	for(int i = 0; i < given_matching.getTenantCount(); i++){
+    		match.add(i, 0);
+    	}
+		
+    	for(int i = 0;i < given_matching.getTenantCount(); i++){
+			int v = setMatch(given_matching, match, i);
+			while(v != -1){
+				v=setMatch(given_matching,match,v);
+			}
+		}
+		for(int i = 0; i < given_matching.getTenantCount(); i++){
+			match.set(hm.get(i),i);
+		}
+		given_matching.setTenantMatching(match);
+
+		return given_matching;
     }
 }
